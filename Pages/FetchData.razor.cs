221d832305e7e2487e3f3c78f55bc33280c0a3db
Boban Spirovski <spiro_dt@hotmail.com>
2020-10-17 15:27:59 +0200

    Add project files.

---
+++ Pages/FetchData.razor.cs
@@ -0,0 +1,19 @@
+﻿using BlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class FetchData
+    {
+        [Inject] WeatherForecastService ForecastService { get; set; }
+        WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+
+        }
+    }
+}
