d6b4b8e950553ba948d9441700249ed72297ef0d
Boban Spirovski <spiro_dt@hotmail.com>
2020-11-02 10:59:30 +0100

    app settings

--- Radzen/BSSR/server/Pages/TblArtikal.razor.cs
+++ Radzen/BSSR/server/Pages/TblArtikal.razor.cs
@@ -9,6 +9,29 @@ namespace Bssr.Pages
 {
     public partial class TblArtikalComponent
     {
+        public TblArtikalComponent()
+        {
+            columns = new[] {
+                new ColumnData { Index = 2,ColumnName = "Naziv", Visible = true, Title = "Назив" },
+                new ColumnData { Index =3, ColumnName = "Sifra", Visible = false,Title = "Шифра" },
+                new ColumnData { Index =1, ColumnName = "AltNaziv", Visible = true,Title = "Алт назив" },
+            };
+        }
+        public bool showIdColumn = true;
+        public class ColumnData
+        {
+            public string ColumnName { get; set; }
+            public string Title { get; set; }
+            public bool Visible { get; set; }
+            public int Index  { get; set; }
+    }
 
+        public IEnumerable<ColumnData> columns;
+
+        protected void OnInitialized()
+        {
+           
+        }
+        
     }
 }
